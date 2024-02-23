5a544626fe472b867a012b2243589678bc41a614
althaf27727 <kmalthaf967@gmail.com>
2024-02-23 11:31:49 +0530

    API constant created

--- myapp/src/Redux/Reducer/doctorSlice.js
+++ myapp/src/Redux/Reducer/doctorSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { BASE_URL } from '../../../constants/API';
 
 const initialState={
     doctorData:[],
@@ -7,7 +8,8 @@ const initialState={
 }
 
 export const getDoctors= createAsyncThunk('doctors/get',async()=>{
-    const response = await axios.get('http://localhost:5000/api/admin/view-doctor')
+    // const response = await axios.get('http://localhost:5000/api/admin/view-doctor')
+    const response = await axios.get(`${BASE_URL}/api/admin/view-doctor`)
 
     const result= response.data.data
     console.log(result);
