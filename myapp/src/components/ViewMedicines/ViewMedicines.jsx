3f06ee944fc57802c51bb1c8568719db7d1be7cf
althaf27727 <kmalthaf967@gmail.com>
2024-02-29 14:14:44 +0530

    CSS Added

--- myapp/src/components/ViewMedicines/ViewMedicines.jsx
+++ myapp/src/components/ViewMedicines/ViewMedicines.jsx
@@ -2,121 +2,80 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { Link } from "react-router-dom";
 import { BASE_URL } from "../../constants/API";
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import './ViewMedicines.css'
 const ViewMedicines = () => {
   const Token = localStorage.getItem("Token");
-  console.log(Token);
   const [data, setData] = useState([]);
+
   useEffect(() => {
+    axios.get(`${BASE_URL}api/medicine/view-medicine`).then((response) => {
+      setData(response.data.data);
+    });
+  }, []);
+
+  const addToMycart = (item) => {
     axios
-      .get("http://localhost:5000/api/medicine/view-medicine")
+      .post(`${BASE_URL}api/mycart/addto-mycart`, item, {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
+      })
       .then((response) => {
-        console.log(response);
         setData(response.data.data);
       });
-  }, []);
-     const addToMycart = (item) => {
-        axios
-        .post(`${BASE_URL}api/mycart/addto-mycart`, item,{
-          headers:{
-            Authorization: `Bearer ${Token}`
-          }
-          
-        })
-
-        .then((response) => {
-          console.log(response);
-          setData(response.data.data);
-         
-      
-        });
-        
-      }
-      
-
-
+  };
 
   const addToWishlist = (item) => {
     axios
-    .post(`${BASE_URL}/api/wishlist/add-wishlist`, item,{
-        headers:{
-            Authorization: `Bearer ${Token}`
-        }
+      .post(`${BASE_URL}/api/wishlist/add-wishlist`, item, {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
       })
-      
       .then((response) => {
-        console.log(response);
         setData(response.data.data);
       });
-
-   
-      
-      
   };
-  //     .post("http://localhost:5000/api/wishlist/add-wishlist", item,{
-  //       headers:{
-  //           Authorization: `Bearer ${Token}`
-  //       }
-  //     })
-      
-  //     .then((response) => {
-  //       console.log(response);
-  //       setData(response.data.data);
-  //     });
 
-   
-      
-      
-  // };
   return (
     <div>
-       <div className="box">
-      <div
-        className="row"
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          marginTop: "30px",
-          maxWidth: "1536px",
-        }}
-      >
-        {data.map((item) => (
-          <Card border="info" style={{ width: "18rem" }}>
-            {/* <Card.Img variant="top" src={`/images/${item.image}`} /> */}
-            <Card.Img  variant="top" src={item.image} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>{item.description}</Card.Text>
-              <Card.Text>{item.price}</Card.Text>
-            </Card.Body>
+      <div className="box">
+        <div className="row">
+          {data.map((item) => (
+            <Card key={item.id} border="info" className="medicine-card">
+              <Card.Img variant="top" src={item.image} />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>{item.description}</Card.Text>
+                <Card.Text>{item.price}</Card.Text>
+              </Card.Body>
 
-            {Token !== null ? (
-              <>
-                <Link to={`/view-wishlist`}>
-                  <Button onClick={() => addToWishlist(item)}>
-                    {" "}
-                    Add to wishlist
-                  </Button>
-                </Link>
-                <br />
-                <Link to={`/view-mycart`}>
-                  <Button onClick={() => addToMycart(item)}>
-                    {" "}
-                    Add to Cart
-                  </Button>
-                </Link>
-              </>
-            ) : (
-              ""
-            )}
-          </Card>
-        ))}
+              {Token !== null ? (
+                <>
+                  <Link to={`/view-wishlist`}>
+                    <Button onClick={() => addToWishlist(item)}>
+                      Add to wishlist
+                    </Button>
+                  </Link>
+                  <br />
+                  <Link to={`/view-mycart`}>
+                    <Button onClick={() => addToMycart(item)}>
+                      Add to Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                ""
+              )}
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+      <ToastContainer />
     </div>
   );
 };
