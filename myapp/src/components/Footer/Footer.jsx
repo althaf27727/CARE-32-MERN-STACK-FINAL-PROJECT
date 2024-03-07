fcfc8a8b0c5e429a11cd136356ca6f217e05ba16
althaf27727 <kmalthaf967@gmail.com>
2024-03-07 06:27:44 +0530

    footerr

--- myapp/src/components/Footer/Footer.jsx
+++ myapp/src/components/Footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./Footer.css"
 
 const Footer = () => {
   return (
-    <div></div>
+
+    <div>
+
+<div className="footer">
+    <h2 >CARE 32 DENTALS</h2>
+    <p>&copy; 2024 Care 32 Dentals. All rights reserved.</p>
+
+  </div>
+    </div>
   )
 }
 
